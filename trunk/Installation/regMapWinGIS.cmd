2b4fa69d9795107da3b6c434272d49c8a738f225
SND\pmeems_cp <SND\pmeems_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2011-12-20 22:21:15 +0000

    Missing files
    git-svn-id: https://mapwingis.svn.codeplex.com/svn@64251 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ trunk/Installation/regMapWinGIS.cmd
@@ -0,0 +1,7 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+rem The lines above are from http://www.codeproject.com/Tips/119828/Running-a-bat-file-as-administrator-Correcting-cur.aspx
+regsvr32 /u /s MapWinGIS.ocx
+regsvr32 /s MapWinGIS.ocx
+setenv -a PROJ_LIB "%cd%\PROJ_NAD"
+setenv -a GDAL_DATA "%cd%\gdal_data"
