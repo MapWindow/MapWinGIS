27d42b8d97d4d3eb58c383f1a4d4cb682830ec5f
SND\pmeems_cp <SND\pmeems_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2013-10-16 11:59:56 +0000

    First version of an application that will test some of the functionality of MapWinGIS. This version only handles the file dropped event and can open shapefiles and grid files, image files raise an error (from MapWinGIS/GDAL).
    git-svn-id: https://mapwingis.svn.codeplex.com/svn@73174 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ trunk/TestApplication/TestApplication/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TestApplication
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+      this.axMap1 = new AxMapWinGIS.AxMap();
+      this.label1 = new System.Windows.Forms.Label();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.splitContainer2.Panel1.SuspendLayout();
+      this.splitContainer2.Panel2.SuspendLayout();
+      this.splitContainer2.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+      this.splitContainer1.Name = "splitContainer1";
+      this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+      this.splitContainer1.Size = new System.Drawing.Size(538, 408);
+      this.splitContainer1.SplitterDistance = 334;
+      this.splitContainer1.TabIndex = 0;
+      // 
+      // splitContainer2
+      // 
+      this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+      this.splitContainer2.Name = "splitContainer2";
+      // 
+      // splitContainer2.Panel1
+      // 
+      this.splitContainer2.Panel1.Controls.Add(this.label1);
+      // 
+      // splitContainer2.Panel2
+      // 
+      this.splitContainer2.Panel2.Controls.Add(this.axMap1);
+      this.splitContainer2.Size = new System.Drawing.Size(538, 334);
+      this.splitContainer2.SplitterDistance = 157;
+      this.splitContainer2.TabIndex = 0;
+      // 
+      // axMap1
+      // 
+      this.axMap1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.axMap1.Enabled = true;
+      this.axMap1.Location = new System.Drawing.Point(0, 0);
+      this.axMap1.Name = "axMap1";
+      this.axMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap1.OcxState")));
+      this.axMap1.Size = new System.Drawing.Size(377, 334);
+      this.axMap1.TabIndex = 0;
+      this.axMap1.FileDropped += new AxMapWinGIS._DMapEvents_FileDroppedEventHandler(this.AxMap1FileDropped);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(47, 16);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Tests";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(538, 408);
+      this.Controls.Add(this.splitContainer1);
+      this.Name = "Form1";
+      this.Text = "Test application for MapWinGIS";
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.ResumeLayout(false);
+      this.splitContainer2.Panel1.ResumeLayout(false);
+      this.splitContainer2.Panel1.PerformLayout();
+      this.splitContainer2.Panel2.ResumeLayout(false);
+      this.splitContainer2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.axMap1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.SplitContainer splitContainer2;
+    private System.Windows.Forms.Label label1;
+    private AxMapWinGIS.AxMap axMap1;
+  }
+}
+
---
+++ trunk/TestApplication/TestApplication/Form1.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="MapWindow Open Source GIS Community">
+//   MapWindow Open Source GIS Community
+// </copyright>
+// <summary>
+//   Form to test some of the functionality of MapWinGIS
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace TestApplication
+{
+  using System;
+  using System.Windows.Forms;
+
+  using MapWinGIS;
+
+  /// <summary>Defines the form</summary>
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Form1"/> class.
+    /// </summary>
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    /// <summary>Handle the dropped file</summary>
+    /// <param name="sender">
+    /// The sender.
+    /// </param>
+    /// <param name="e">
+    /// The e.
+    /// </param>
+    private void AxMap1FileDropped(object sender, AxMapWinGIS._DMapEvents_FileDroppedEvent e)
+    {
+      // Check extension:
+      var ext = System.IO.Path.GetExtension(e.filename);
+      if (ext == null)
+      {
+        return;
+      }
+
+      // Load shapefile
+      if (ext == ".shp")
+      {
+        var sf = new Shapefile();
+        if (!sf.Open(e.filename, null))
+        {
+          var msg = string.Format("Error opening shapefile: {0}", sf.get_ErrorMsg(sf.LastErrorCode));
+          System.Diagnostics.Debug.WriteLine(msg);
+          MessageBox.Show(msg);
+        }
+        else
+        {
+          axMap1.AddLayer(sf, true);
+        }
+
+        return;
+      }
+      
+      // Track GDAL Errors:
+      var settings = new GlobalSettings();
+
+      // Load grid file
+      var grd = new Grid();
+      if (grd.CdlgFilter.Contains(ext))
+      {
+        settings.ResetGdalError();
+        if (!grd.Open(e.filename, GridDataType.UnknownDataType, true, GridFileType.UseExtension, null))
+        {
+          var msg = string.Format(
+            "Error opening grid: {0}{1}Gdal Error: {2}", 
+            grd.get_ErrorMsg(grd.LastErrorCode), 
+            Environment.NewLine, 
+            settings.GdalLastErrorMsg);
+          System.Diagnostics.Debug.WriteLine(msg);
+          MessageBox.Show(msg);
+        }
+        else
+        {
+          axMap1.AddLayer(grd, true);
+        }
+
+        return;
+      }
+
+      // Load image file
+      var img = new Image();
+      if (img.CdlgFilter.Contains(ext))
+      {
+        settings.ResetGdalError();
+        if (!img.Open(e.filename, ImageType.USE_FILE_EXTENSION, false, null))
+        {
+          var msg = string.Format(
+            "Error opening image: {0}{1}Gdal Error: {2}",
+            img.get_ErrorMsg(img.LastErrorCode),
+            Environment.NewLine,
+            settings.GdalLastErrorMsg);
+          System.Diagnostics.Debug.WriteLine(msg);
+          MessageBox.Show(msg);
+        }
+        else
+        {
+          axMap1.AddLayer(img, true);
+        }
+
+        return;
+      }
+
+      // Don't know how to handle the dropped file:
+      return;
+    }
+  }
+}
