35b05c23b28c5d6f9e4a6f8224ad1b90fc3f334e
SND\heste015_cp <SND\heste015_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2014-04-19 13:46:25 +0000

    Google Mercator projection is initialized using WKT string instead of Proj.4 string.
    git-svn-id: https://mapwingis.svn.codeplex.com/svn@74228 142e9c35-602f-4880-9f37-ec48a24daa6a

--- trunk/COM classes/GeoProjection.cpp
+++ trunk/COM classes/GeoProjection.cpp
@@ -1022,7 +1022,31 @@ STDMETHODIMP CGeoProjection::StopTransform()
 STDMETHODIMP CGeoProjection::SetGoogleMercator(VARIANT_BOOL* retVal)
 {
 	AFX_MANAGE_STATE(AfxGetStaticModuleState());
-	this->ImportFromProj4(A2BSTR("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"), retVal);
+
+	this->ImportFromWKT(A2BSTR(
+		"PROJCS[\"WGS 84 / Pseudo-Mercator\","
+			"GEOGCS[\"WGS 84\","
+				"DATUM[\"WGS_1984\","
+					"SPHEROID[\"WGS 84\",6378137,298.257223563,"
+						"AUTHORITY[\"EPSG\",\"7030\"]],"
+					"AUTHORITY[\"EPSG\",\"6326\"]],"
+				"PRIMEM[\"Greenwich\",0,"
+					"AUTHORITY[\"EPSG\",\"8901\"]],"
+				"UNIT[\"degree\",0.0174532925199433,"
+					"AUTHORITY[\"EPSG\",\"9122\"]],"
+				"AUTHORITY[\"EPSG\",\"4326\"]],"
+			"PROJECTION[\"Mercator_1SP\"],"
+			"PARAMETER[\"central_meridian\",0],"
+			"PARAMETER[\"scale_factor\",1],"
+			"PARAMETER[\"false_easting\",0],"
+			"PARAMETER[\"false_northing\",0],"
+			"UNIT[\"metre\",1,"
+				"AUTHORITY[\"EPSG\",\"9001\"]],"
+			"AXIS[\"X\",EAST],"
+			"AXIS[\"Y\",NORTH],"
+			"EXTENSION[\"PROJ4\",\"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs\"],"
+			"AUTHORITY[\"EPSG\",\"3857\"]]"), retVal);
+
 	return S_OK;
 }
 
@@ -1063,7 +1087,7 @@ STDMETHODIMP CGeoProjection::TryAutoDetectEpsg(int* epsgCode, VARIANT_BOOL* retV
 	else
 	{
 		const char *pszAuthName = m_projection->GetAuthorityName( "PROJCS" );
-		if( pszAuthName != NULL && _strnicmp(pszAuthName, "epsg", 4))
+		if( pszAuthName != NULL && !_strnicmp(pszAuthName, "epsg", 4))
 		    *epsgCode = atoi(m_projection->GetAuthorityCode( "PROJCS" ));
 	}
 	*retVal = *epsgCode != -1 ? VARIANT_TRUE: VARIANT_FALSE;
