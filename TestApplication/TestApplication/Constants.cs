6e6aed2a50ffb2f9dd845a6ba37d6ce6638bccbf
SND\pmeems_cp <SND\pmeems_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2014-11-16 15:56:15 +0000

    Moved TestApplication to Root/NET Assemblies/ folder
    git-svn-id: https://mapwingis.svn.codeplex.com/svn@75170 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ TestApplication/TestApplication/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TestApplication
+{
+    public static class Constants
+    {
+        public const string SCRIPT_DATA_PATH = @"d:\mw\testingscripts\scriptdata\general";
+        public const string DATA_FILES_PATH = @"D:\dev\MapWinGIS\trunk\TestApplication\TestApplication\data";
+    }
+}
