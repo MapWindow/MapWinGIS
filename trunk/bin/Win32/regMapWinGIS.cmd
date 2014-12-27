bcdf7a558b54ba331e30e4931141a98cddb733a1
SND\pmeems_cp <SND\pmeems_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2014-12-27 16:34:39 +0000

    The last files needed for MapWinGIS v4.9.3 and MapWindowLite v1.0
    git-svn-id: https://mapwingis.svn.codeplex.com/svn@75411 142e9c35-602f-4880-9f37-ec48a24daa6a

--- trunk/bin/Win32/regMapWinGIS.cmd
+++ trunk/bin/Win32/regMapWinGIS.cmd
@@ -4,5 +4,5 @@ rem The lines above are from http://www.codeproject.com/Tips/119828/Running-a-ba
 regsvr32 /u /s MapWinGIS.ocx
 regsvr32 MapWinGIS.ocx
 setenv -a PROJ_LIB "%cd%\..\PROJ_NAD"
-setenv -a GDAL_DATA "%cd%\..\gdal-data"
+setenv -a GDAL_DATA "%cd%\gdal-data"
 
