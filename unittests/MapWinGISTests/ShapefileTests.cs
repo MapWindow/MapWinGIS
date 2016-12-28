77d2f50c6789e008f328805c2eddca595299397d
Paul Meems <p.meems@akkerweb.nl>
2016-12-28 12:12:45 +0100

    Unit test for MWGIS-55: sf.Save() looses newly added fields.

---
+++ unittests/MapWinGISTests/ShapefileTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using MapWinGIS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MapWinGISTests
+{
+    [TestClass]
+    public class ShapefileTests
+    {
+
+        [TestMethod]
+        public void SaveShapefileTest()
+        {
+            var tempFolder = Path.GetTempPath();
+            var tempFilename = Path.Combine(tempFolder, "CreateShapefileTest.shp");
+            DeleteShapefile(tempFilename);
+
+            bool result;
+            // Create shapefile
+            var sf = new ShapefileClass();
+            try
+            {
+                result = sf.CreateNewWithShapeID(tempFilename, ShpfileType.SHP_POINT);
+                Assert.IsTrue(result, "Could not create shapefile");
+
+                Assert.IsTrue(sf.EditingShapes, "Shapefile is not in edit shapes mode");
+                Assert.IsTrue(sf.EditingTable, "Shapefile is not in edit table mode");
+
+                // Add fields:
+                var fieldIndex = sf.EditAddField("date", FieldType.STRING_FIELD, 50, 0);
+                Assert.AreEqual(1, fieldIndex, "Could not add field");
+                fieldIndex = sf.EditAddField("remarks", FieldType.STRING_FIELD, 100, 0);
+                Assert.AreEqual(2, fieldIndex, "Could not add field");
+                fieldIndex = sf.EditAddField("amount", FieldType.INTEGER_FIELD, 3, 0);
+                Assert.AreEqual(3, fieldIndex, "Could not add field");
+                Assert.AreEqual(fieldIndex + 1, sf.NumFields, "Number of fields are incorrect");
+
+                result = sf.Save();
+                Assert.IsTrue(result, "Could not save shapefile");
+                Assert.AreEqual(fieldIndex + 1, sf.NumFields, "Number of fields are incorrect");
+            }
+            finally
+            {
+                // Close the shapefile:
+                result = sf.Close();
+                Assert.IsTrue(result, "Could not close shapefile");
+            }
+        }
+
+        [TestMethod]
+        public void CreateShapefileTest()
+        {
+            var tempFolder = Path.GetTempPath();
+            var tempFilename = Path.Combine(tempFolder, "CreateShapefileTest.shp");
+            DeleteShapefile(tempFilename);
+
+            bool result;
+            // Create shapefile
+            var sf = new ShapefileClass();
+            try
+            {
+                result = sf.CreateNewWithShapeID(tempFilename, ShpfileType.SHP_POINT);
+                Assert.IsTrue(result, "Could not create shapefile");
+
+                Assert.IsTrue(sf.EditingShapes, "Shapefile is not in edit shapes mode");
+                Assert.IsTrue(sf.EditingTable, "Shapefile is not in edit table mode");
+
+                // Add fields:
+                var fieldIndex = sf.EditAddField("date", FieldType.STRING_FIELD, 50, 0);
+                Assert.AreEqual(1, fieldIndex, "Could not add field");
+                fieldIndex = sf.EditAddField("remarks", FieldType.STRING_FIELD, 100, 0);
+                Assert.AreEqual(2, fieldIndex, "Could not add field");
+                fieldIndex = sf.EditAddField("amount", FieldType.INTEGER_FIELD, 3, 0);
+                Assert.AreEqual(3, fieldIndex, "Could not add field");
+                Assert.AreEqual(fieldIndex + 1, sf.NumFields, "Number of fields are incorrect");
+
+                result = sf.Save();
+                Assert.IsTrue(result, "Could not save shapefile");
+                Assert.AreEqual(fieldIndex + 1, sf.NumFields, "Number of fields are incorrect");
+
+                // Create shape:
+                var shp = new ShapeClass();
+                result = shp.Create(sf.ShapefileType);
+                Assert.IsTrue(result, "Could not create shape");
+                // Create point:
+                var pnt = new Point
+                {
+                    x = 200,
+                    y = 200
+                };
+                // Add point:
+                var pointIndex = shp.numPoints;
+                result = shp.InsertPoint(pnt, ref pointIndex);
+                Assert.IsTrue(result, "Could not insert point");
+                var shapeIndex = sf.NumShapes;
+                result = sf.EditInsertShape(shp, ref shapeIndex);
+                Assert.IsTrue(result, "Could not insert shape");
+
+
+                // Check shapefile:
+                Assert.AreEqual(shapeIndex + 1, sf.NumShapes, "Number of shapes are incorrect");
+                Assert.AreEqual(fieldIndex + 1, sf.NumFields, "Number of fields are incorrect");
+            }
+            finally
+            {
+                // Close the shapefile:
+                result = sf.Close();
+                Assert.IsTrue(result, "Could not close shapefile");
+            }
+        }
+
+        private static void DeleteShapefile(string filename)
+        {
+            var folder = Path.GetDirectoryName(filename);
+            if (folder == null) return;
+            var filenameBody = Path.GetFileNameWithoutExtension(filename);
+            foreach (var f in Directory.EnumerateFiles(folder, filenameBody + ".*"))
+            {
+                Debug.WriteLine("deleting " + f);
+                File.Delete(f);
+            }
+        }
+    }
+}
