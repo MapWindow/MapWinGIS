6bf4991e7b632ce36374c1e9984c3ae927296a2b
Paul Meems <p.meems@akkerweb.nl>
2017-01-02 16:09:47 +0100

    Add HDF5 dlls to copy script.

--- src/CopyTamasFiles.bat
+++ src/CopyTamasFiles.bat
@@ -16,7 +16,7 @@ set _to_dir=%2
 if '%_from_dir%'=='' if '%_to_dir%'=='' GOTO usage
 
 REM Copy gdal plugins:
-FOR %%G IN (gdal_MrSID.dll gdal_netCDF.dll) DO (
+FOR %%G IN (gdal_MrSID.dll gdal_netCDF.dll gdal_HDF5.dll gdal_HDF5Image.dll) DO (
 	IF EXIST %_from_dir%\gdal\plugins\%%G (
     xcopy /v /c /r /y %_from_dir%\gdal\plugins\%%G  %_to_dir%gdalplugins\
 	)
@@ -37,7 +37,7 @@ REM Copy needed Tamas binaries:
 FOR %%G IN (cfitsio.dll geos.dll geos_c.dll hdf5.dll hdf5_hl.dll hdf5_cpp.dll hdf5_hl_cpp.dll libcurl.dll 
             iconv.dll libeay32.dll libmysql.dll libpq.dll libxml2.dll lti_dsdk_9.1.dll
             lti_lidar_dsdk_1.1.dll libecwj2.dll netcdf.dll openjp2.dll proj.dll spatialite.dll sqlite3.dll freexl.dll  
-            ssleay32.dll szip.dll tbb.dll xdrdll.dll xerces-c_2_8.dll xerces-c_3_1.dll zlib1.dll libtiff.dll expat.dll) DO (
+            ssleay32.dll szip.dll tbb.dll xdrdll.dll xerces-c_3_1.dll zlib1.dll libtiff.dll expat.dll) DO (
 	IF EXIST %_from_dir%\%%G (
     xcopy /v /c /r /y %_from_dir%\%%G  %_to_dir%
 	)
