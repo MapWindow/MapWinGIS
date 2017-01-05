d8fe8fd05b7dd062836a6b5a53252192d2039b24
Paul Meems <p.meems@akkerweb.nl>
2017-01-05 22:00:58 +0100

    Unit test for CORE-177

--- unittests/MapWinGISTests/GdalTests.cs
+++ unittests/MapWinGISTests/GdalTests.cs
@@ -44,7 +44,7 @@ namespace MapWinGISTests
                 Debug.WriteLine("odcCreateGeomFieldAfterCreateLayer: " + capability);
                 // Assert.IsTrue(capability, "Cannot create GeomField After CreateLayer");
 
-                TestLayers(ogrDatasource);
+                TestSQLiteLayers(ogrDatasource);
             }
             finally
             {
@@ -52,6 +52,49 @@ namespace MapWinGISTests
             }
         }
 
+        /// <summary>
+        /// Opens the post gis different port test.
+        /// </summary>
+        /// <remarks>Because it needs an existing host, database and table it is not a 'proper' test by default</remarks>
+        // [TestMethod]
+        public void OpenPostGISDifferentPortTest()
+        {
+            var ogrDatasource = new OgrDatasourceClass();
+            try
+            {
+                var result = ogrDatasource.Open("PG:host=127.0.0.1 port=55432 dbname=aw_croppingscheme user=aw_croppingschem password=test123");
+                Assert.IsTrue(result, "Cannot open PostGIS Connectie: " + ogrDatasource.GdalLastErrorMsg);
+                var settings = new GlobalSettings();
+
+                var capability = ogrDatasource.TestCapability(tkOgrDSCapability.odcCreateLayer);
+                Debug.WriteLine("odcCreateLayer: " + capability);
+                Assert.IsTrue(capability, "Cannot create layer");
+                capability = ogrDatasource.TestCapability(tkOgrDSCapability.odcDeleteLayer);
+                Debug.WriteLine("odcDeleteLayer: " + capability);
+                Assert.IsTrue(capability, "Cannot delete layer");
+                capability = ogrDatasource.TestCapability(tkOgrDSCapability.odcCreateDataSource);
+                Debug.WriteLine("odcCreateDataSource: " + capability);
+                //Assert.IsTrue(capability), "Cannot create datasource");
+                capability = ogrDatasource.TestCapability(tkOgrDSCapability.odcDeleteDataSource);
+                Debug.WriteLine("odcDeleteDataSource: " + capability);
+                //Assert.IsTrue(capability, "Cannot delete datasource");
+                capability = ogrDatasource.TestCapability(tkOgrDSCapability.odcCreateGeomFieldAfterCreateLayer);
+                Debug.WriteLine("odcCreateGeomFieldAfterCreateLayer: " + capability);
+                Assert.IsTrue(capability, "Cannot create GeomField After CreateLayer");
+
+                Assert.IsTrue(ogrDatasource.LayerCount > 1, "No layers found");
+
+                for (var i = 0; i < ogrDatasource.LayerCount; i++)
+                {
+                    var layer = ogrDatasource.GetLayer(i);
+                    Debug.WriteLine(layer.Name);
+                }
+            }
+            finally
+            {
+                ogrDatasource.Close();
+            }
+        }
         [TestMethod]
         public void CreateLayerSQLiteTest()
         {
@@ -89,7 +132,7 @@ namespace MapWinGISTests
                 var numFeatures = newLayer.FeatureCount[true];
                 Debug.WriteLine("numFeatures: " + numFeatures);
 
-                TestLayers(ogrDatasource);
+                TestSQLiteLayers(ogrDatasource);
             }
             finally
             {
@@ -104,9 +147,9 @@ namespace MapWinGISTests
         /// </summary>
         /// <remarks>https://mapwindow.atlassian.net/browse/MWGIS-56</remarks>
         [TestMethod]
-        public void OpenHdf5()
+        public void GdalInfoHdf5()
         {
-            const string filename = "HDF5/test.h5";
+            const string filename = @"HDF5\test.h5";
             // Check if GDAL can open it:
             var utils = new UtilsClass();
             var settings = new GlobalSettings();
@@ -125,7 +168,80 @@ namespace MapWinGISTests
             // TODO: Open subdataset as grid:
         }
 
-        private static void TestLayers(IOgrDatasource ogrDatasource)
+        /// <summary>
+        /// Reads the attributes from a postGIS layer.
+        /// </summary>
+        /// <remarks>https://mapwindow.atlassian.net/browse/CORE-177</remarks>
+        [TestMethod]
+        public void ReadAttributesFromPostGISLayer()
+        {
+            var ogrDatasource = new OgrDatasourceClass();
+            try
+            {
+                var result = ogrDatasource.Open("PG:host=127.0.0.1 port=5432 dbname=mw_test user=mapwindow password=test123");
+                Assert.IsTrue(result, "Cannot open PostGIS Connectie: " + ogrDatasource.GdalLastErrorMsg);
+                // var settings = new GlobalSettings();
+
+                Assert.IsTrue(ogrDatasource.LayerCount > 1, "No layers found");
+
+                OgrLayer attributeLayer = null;
+                for (var i = 0; i < ogrDatasource.LayerCount; i++)
+                {
+                    var layer = ogrDatasource.GetLayer(i);
+                    Debug.WriteLine(layer.Name);
+                    if (layer.Name == "attributes") attributeLayer = layer;
+                }
+
+                Assert.IsNotNull(attributeLayer, "Couldn't find the attribute layer");
+                Debug.WriteLine("Working with attributes layer");
+
+                var sf = attributeLayer.GetBuffer();
+                Assert.IsNotNull(sf, "Could not get buffer");
+
+                var numShapes = sf.NumShapes;
+                Assert.AreEqual(3, numShapes);
+
+                var numFields = sf.NumFields;
+                Assert.IsTrue(numFields>0, "No fields found");
+
+                // First shape has all attributes filled:
+                Debug.WriteLine("Testing first shape");
+                for (var fieldIndex = 0; fieldIndex < numFields; fieldIndex++)
+                {
+                    var value = sf.CellValue[fieldIndex, 0];
+                    Debug.WriteLine($"{sf.Field[fieldIndex].Name}: {value}");
+                    Assert.IsNotNull(value, $"{sf.Field[fieldIndex].Name} should not be null");
+                }
+
+                // Second shape has only the geometry filled and the rest default values (which should be NULL):
+                Debug.WriteLine("Testing second shape");
+                var numNonNulls = 0;
+                for (var fieldIndex = 0; fieldIndex < numFields; fieldIndex++)
+                {
+                    var value = sf.CellValue[fieldIndex, 1];
+                    Debug.WriteLine($"{sf.Field[fieldIndex].Name}: {value}");
+                    if (!string.IsNullOrEmpty(value.ToString())) numNonNulls++;
+                }
+                if (numNonNulls > 0) Debug.WriteLine($"Error! Second shape has {numNonNulls} non NULL fields!");
+
+                // Third shape has all fields explicitly NULL:
+                Debug.WriteLine("Testing third shape");
+                numNonNulls = 0;
+                for (var fieldIndex = 0; fieldIndex < numFields; fieldIndex++)
+                {
+                    var value = sf.CellValue[fieldIndex, 2];
+                    Debug.WriteLine($"{sf.Field[fieldIndex].Name}: {value}");
+                    if (!string.IsNullOrEmpty(value.ToString())) numNonNulls++;
+                }
+                Assert.AreEqual(0, numNonNulls, $"Third shape has {numNonNulls} non NULL fields!");
+            }
+            finally
+            {
+                ogrDatasource.Close();
+            }
+        }
+
+        private static void TestSQLiteLayers(IOgrDatasource ogrDatasource)
         {
             // Get layers:
             var lastLayername = string.Empty;
@@ -139,8 +255,8 @@ namespace MapWinGISTests
                 Debug.WriteLine("Driver Name: " + layer.DriverName);
                 Assert.AreEqual("SQLite", layer.DriverName, "Wrong driver name");
                 var capability = layer.TestCapability(tkOgrLayerCapability.olcRandomRead);
-                Assert.IsTrue(capability, "Cannot random read");
                 Debug.WriteLine("olcRandomRead: " + capability);
+                Assert.IsTrue(capability, "Cannot random read");
                 capability = layer.TestCapability(tkOgrLayerCapability.olcRandomWrite);
                 Debug.WriteLine("olcRandomWrite: " + capability);
                 Assert.IsTrue(capability, "Cannot random write");
