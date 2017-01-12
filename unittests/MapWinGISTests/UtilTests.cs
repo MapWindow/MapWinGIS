4e8b6656080eac1186941fd432b549f6c39559c8
Paul Meems <p.meems@topx-group.nl>
2017-01-12 16:41:00 +0100

    Updated unit test for Utils.CalculateRaster()

--- unittests/MapWinGISTests/UtilTests.cs
+++ unittests/MapWinGISTests/UtilTests.cs
@@ -22,12 +22,28 @@ namespace MapWinGISTests
         {
             var utils = new UtilsClass();
             var tmpFile = Path.GetTempFileName();
+            const string tiffInput = "GeoTiff/5band.tif";
             string errorMsg;
-            const string formula = "([5band.tif@5]-[5band.tif@3])/([5band.tif@5]+[5band.tif@3])";
+            var formula = $"([{tiffInput}@5]-[{tiffInput}@3])/([{tiffInput}@5]+[{tiffInput}@3])";
             // TODO: Can't use only one file:
-            var result = utils.CalculateRaster(new[] {@"GeoTiff/5band.tif", "HDF5/test.h5" }, formula, tmpFile, "GTiff", 0f, null, out errorMsg);
-            // TODO: throws a memory error
+            var result = utils.CalculateRaster(new[] { tiffInput, "GeoTiff/5bandCopy.tif" }, formula, tmpFile, "GTiff", 0f, null, out errorMsg);
             Assert.IsTrue(result, "utils.CalculateRaster was unsuccessful: " + errorMsg);
+            Debug.WriteLine(tmpFile);
+            
+            var tiffIn = new ImageClass();
+            result = tiffIn.Open(tiffInput);
+            Assert.IsTrue(result, "Cannot open input tiff: " + tiffIn.ErrorMsg[tiffIn.LastErrorCode]);
+
+            var tiffOut = new ImageClass();
+            result = tiffOut.Open(tmpFile, ImageType.TIFF_FILE);
+            Assert.IsTrue(result, "Cannot open output tiff: " + tiffOut.ErrorMsg[tiffOut.LastErrorCode]);
+
+            // Check the output file with the input file:
+            Assert.AreEqual(tiffIn.Extents.xMin, tiffOut.Extents.xMin, "Extents.xMin are not equal");
+            Assert.AreEqual(tiffIn.Extents.xMax, tiffOut.Extents.xMax, "Extents.xMax are not equal");
+            Assert.AreEqual(tiffIn.Extents.yMin, tiffOut.Extents.yMin, "Extents.yMin are not equal");
+            Assert.AreEqual(tiffIn.Extents.yMax, tiffOut.Extents.yMax, "Extents.yMax are not equal");
+            Assert.IsTrue(tiffIn.GeoProjection.IsSame[tiffOut.GeoProjection], "Projections are not the same");
         }
     }
 }
