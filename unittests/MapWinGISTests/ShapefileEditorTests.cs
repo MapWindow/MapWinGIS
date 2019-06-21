8a7b013ecb1f293a225482a2e02479dda737c8b3
Paul Meems <p.meems@topx-group.nl>
2019-06-21 17:21:40 +0200

    Adding unit test for MWGIS-174

--- unittests/MapWinGISTests/ShapefileEditorTests.cs
+++ unittests/MapWinGISTests/ShapefileEditorTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using AxMapWinGIS;
 using MapWinGIS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,14 +10,22 @@ namespace MapWinGISTests
     [TestClass]
     public class ShapefileEditorTests
     {
-        private const string WorkingFolder = @"D:\dev\GIS-data\Joe Rose\Ayrshire\";
+        private AxMap _axMap1;
+
+        [TestInitialize]
+        public void Init()
+        {
+            Helper.DebugMsg("Test was run at " + DateTime.Now);
+            _axMap1 = Helper.GetAxMap(false);
+        }
 
         [TestMethod]
         public void Simplify()
         {
             var utils = new Utils();
+            const string workingFolder = @"D:\dev\GIS-data\Joe Rose\Ayrshire\";
 
-            var sfOriginal = Helper.OpenShapefile(Path.Combine(WorkingFolder, "Parish_Kirkoswald.shp"));
+            var sfOriginal = Helper.OpenShapefile(Path.Combine(workingFolder, "Parish_Kirkoswald.shp"));
             Debug.WriteLine($"Original has {sfOriginal.NumShapes} shapes");
             Debug.WriteLine($"Original first shape has {sfOriginal.Shape[0].numPoints} points.");
 
@@ -45,6 +55,33 @@ namespace MapWinGISTests
             sfSimplifyLines.Close();
 
             sfOriginal.Close();
+        }
+
+        [TestMethod]
+        public void EditAddShapeTest()
+        {
+            // https://mapwindow.discourse.group/t/system-accessviolationexception-after-editaddshape/196
+            var pnt = new Point { x = 6.7369281, y = 53.1603648 };
+
+            var shp = new Shape();
+            shp.Create(ShpfileType.SHP_POLYGON);
+            shp.InsertPoint(pnt, 0);
+
+            var sf = new Shapefile();
+            var result = sf.CreateNewWithShapeID("", ShpfileType.SHP_POLYGON);
+            Assert.IsTrue(result, "CreateNewWithShapeID failed: " + sf.ErrorMsg[sf.LastErrorCode]);
+
+            var shpIndex = sf.EditAddShape(shp);
+            Assert.IsTrue(shpIndex > -1, "EditAddShape failed: " + sf.ErrorMsg[sf.LastErrorCode]);
+
+            sf.InteractiveEditing = true;
+            Assert.IsTrue(sf.InteractiveEditing, "InteractiveEditing failed: " + sf.ErrorMsg[sf.LastErrorCode]);
+
+            var layerIndex = _axMap1.AddLayer(sf, true);
+            Assert.IsTrue(layerIndex > -1, "AddLayer failed: " + sf.ErrorMsg[sf.LastErrorCode]);
+
+            result = _axMap1.ShapeEditor.StartEdit(layerIndex, shpIndex);
+            Assert.IsTrue(result, "ShapeEditor.StartEdit failed: " + sf.ErrorMsg[sf.LastErrorCode]);
         }
     }
 }
