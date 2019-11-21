07ab94a0d34b5a2f10952084b6875655abe25fd0
Mathijs Dumon <mathijs.Dumon@gmail.com>
2019-11-21 10:37:52 +0100

    Fixing invalid shape types when using StartNewBoundShape for ogr layers with an active shape type

--- src/Editor/Digitizer.h
+++ src/Editor/Digitizer.h
@@ -3,7 +3,7 @@
 class Digitizer
 {
 public:
-	static void StartNewBoundShape(CShapeEditor* editor, IShapefile* sf, long layerHandle);
+    static void StartNewBoundShape(CShapeEditor* editor, ShpfileType shpType, IShapeDrawingOptions* options, long layerHandle);
 	static bool OnMouseDown(CShapeEditor* editor, double projX, double projY, bool ctrl);
 	
 };
