465ad464f164cbe32059c30f6ad407e4a93bbaa3
Jerry Faust <jerryfaust1@gmail.com>
2020-08-14 20:29:16 -0700

    MWGIS-152; support for local file-based tile provider;

--- src/Tiles/Providers/BaseProvider.cpp
+++ src/Tiles/Providers/BaseProvider.cpp
@@ -28,6 +28,7 @@ CString BaseProvider::_proxyUsername = "";
 CString BaseProvider::_proxyPassword = "";
 CString BaseProvider::_proxyDomain = "";
 CCriticalSection BaseProvider::_clientLock;
+const CString filePrefix = "file:///";
 
 // ************************************************************
 //		GetTileImage()
@@ -92,6 +93,34 @@ CMemoryBitmap* BaseProvider::GetTileHttpData(CString url, CString shortUrl, bool
 }
 
 // ************************************************************
+//		GetTileFileData()
+// ************************************************************
+CMemoryBitmap* BaseProvider::GetTileFileData(CString url)
+{
+	url.Delete(0, filePrefix.GetLength());
+	url.Replace("|", ":");
+	url.Replace("/", "\\");
+
+	std::ifstream fl = std::ifstream(url.GetBuffer(), std::ofstream::binary);
+	if (!fl)
+		return nullptr;
+
+	fl.seekg(0, std::ios::end);
+	int sz = fl.tellg();
+	if (sz == 0)
+		return nullptr;
+
+	std::vector<char> buf(sz);
+
+	fl.seekg(0, std::ios::beg);
+	fl.read(buf.data(), buf.size());
+	if (!fl)
+		return nullptr;
+
+	return ReadBitmap(buf.data(), buf.size());
+}
+
+// ************************************************************
 //		DownloadBitmap()
 // ************************************************************
 CMemoryBitmap* BaseProvider::DownloadBitmap(CPoint& pos, int zoom)
@@ -101,9 +130,10 @@ CMemoryBitmap* BaseProvider::DownloadBitmap(CPoint& pos, int zoom)
 
     shortUrl.Format(R"(\zoom=%d\x=%d\y=%d)", zoom, pos.x, pos.y);
 
-    CMemoryBitmap* bmp = GetTileHttpData(url, shortUrl);
-
-    return bmp;
+	if (url.Find(filePrefix) == 0)
+		return GetTileFileData(url);
+	else
+		return GetTileHttpData(url, shortUrl);
 }
 
 // ************************************************************
