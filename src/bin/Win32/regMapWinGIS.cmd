dbdc0b65148f9940becdbc3bd88ea6ff046062dd
Paul Meems <bontepaarden@gmail.com>
2022-01-25 16:58:43 +0100

    Clean up

--- src/bin/Win32/regMapWinGIS.cmd
+++
@@ -1,9 +0,0 @@
-@setlocal enableextensions
-@cd /d "%~dp0"
-rem The lines above are from http://www.codeproject.com/Tips/119828/Running-a-bat-file-as-administrator-Correcting-cur.aspx
-
-REM Unregister any previous versions:
-%systemroot%\SysWoW64\regsvr32 /u /s MapWinGIS.ocx
-REM Register current version:
-%systemroot%\SysWoW64\regsvr32 MapWinGIS.ocx
-pause
