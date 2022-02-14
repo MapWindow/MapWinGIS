169dbf7a952259927945151833413e238cef01f3
Paul Meems <bontepaarden@gmail.com>
2022-02-14 14:14:30 +0100

    Unit test for MWGIS-278: Add ProjPath to GlobalSettings

---
+++ unittest-net6/UnitTests/GlobalSettingsTests.cs
@@ -0,0 +1,55 @@
+﻿namespace unittest_net6;
+
+public class GlobalSettingsTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public GlobalSettingsTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void GdalDataPathTest()
+    {
+        var gs = new GlobalSettings();
+        gs.ShouldNotBeNull();
+        var gdalDataPath = gs.GdalDataPath;
+        _testOutputHelper.WriteLine(gdalDataPath);
+        gdalDataPath.ShouldNotBeNullOrEmpty("GdalDataPath is not set");
+        gdalDataPath.EndsWith("\\gdal-data\\").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void GdalPluginPathTest()
+    {
+        var gs = new GlobalSettings();
+        gs.ShouldNotBeNull();
+        var gdalPluginPath = gs.GdalPluginPath;
+        _testOutputHelper.WriteLine(gdalPluginPath);
+        gdalPluginPath.ShouldNotBeNullOrEmpty("GdalPluginPath is not set");
+        gdalPluginPath.EndsWith("\\gdalplugins\\").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ProjPathTest()
+    {
+        var gs = new GlobalSettings();
+        gs.ShouldNotBeNull();
+        var projPath = gs.ProjPath;
+        _testOutputHelper.WriteLine("projPath: " + projPath);
+        projPath.ShouldNotBeNullOrEmpty("ProjPath is not set");
+        projPath.EndsWith("\\proj7\\share").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void GdalVersionTest()
+    {
+        var gs = new GlobalSettings();
+        gs.ShouldNotBeNull();
+        var gdalVersion = gs.GdalVersion;
+        _testOutputHelper.WriteLine(gdalVersion);
+        gdalVersion.ShouldNotBeNullOrEmpty("GdalVersion is not set");
+        gdalVersion.StartsWith("GDAL 3.4").ShouldBeTrue();
+    }
+}
