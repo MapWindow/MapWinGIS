821f22a4e9a36bf26977863adba9a7b81260b3a3
Daniel Hedén <daniel.heden@sweco.se>
2026-07-04 19:23:36 +0200

    Updated for GDAL v3.10.3 and proj9

--- src/CopyTamasFiles.bat
+++ src/CopyTamasFiles.bat
@@ -10,6 +10,7 @@ REM * Paul Meems, update for ecw dll, June 2015                 *
 REM * Paul Meems, update for ecw dll to v5.3, Aug 2017          *
 REM * Paul Meems, update for xerces and lti_dsdk dll, Aug 2018  *
 REM * Paul Meems, update for GDAL v3+, Jan 2022                 *
+REM * Daniel Hedén, update for GDAL v3.10.3, proj9              *
 REM * Usage to test:                                            *
 REM * CopyTamasFiles.bat D:\dev\MapwinGIS\GitHub\support\GDAL_SDK\v140\bin\win32 D:\dev\MapwinGIS\GitHub\src\bin\Win32\
 REM *************************************************************
@@ -19,7 +20,7 @@ set _to_dir=%2
 if '%_from_dir%'=='' if '%_to_dir%'=='' GOTO usage
 
 REM Copy gdal plugins:
-FOR %%G IN (gdal_MrSID.dll gdal_netCDF.dll gdal_HDF4.dll gdal_HDF4Image.dll gdal_HDF5.dll gdal_HDF5Image.dll gdal_ECW_JP2ECW.dll gdal_MG4Lidar.dll) DO (
+FOR %%G IN (gdal_ECW_JP2ECW.dll gdal_FITS.dll gdal_GEOR.dll gdal_GIF.dll gdal_HDF4.dll gdal_HDF5.dll gdal_KEA.dll gdal_MG4Lidar.dll gdal_MrSID.dll gdal_PDF.dll gdal_netCDF.dll ogr_OCI.dll ogr_ODBC.dll) DO (
 	IF EXIST %_from_dir%\gdal\plugins\%%G (
     xcopy /v /c /r /y %_from_dir%\gdal\plugins\%%G  %_to_dir%gdalplugins\
 	)
@@ -37,13 +38,25 @@ xcopy /v /c /r /y %_from_dir%\gdal-data\*.* %_to_dir%gdal-data\
 REM xcopy /v /c /r /y %_from_dir%\proj\SHARE\*.* %_to_dir%..\PROJ_NAD\
 
 REM Copy Proj7 data. TODO: Check if copied to correct location:
-xcopy /v /c /r /y %_from_dir%\proj7\share\*.* %_to_dir%proj7\share\
+rem xcopy /v /c /r /y %_from_dir%\proj7\share\*.* %_to_dir%proj7\share\
+
+REM Copy Proj9 data. TODO: Check if copied to correct location:
+xcopy /v /c /r /y %_from_dir%\proj9\share\*.* %_to_dir%proj9\share\
+
+REM Copy gdal plugins
+xcopy /v /c /r /y %_from_dir%\gdal\plugins\*.* %_to_dir%gdal\plugins\
+
+REM Copy gdal plugins-external
+xcopy /v /c /r /y %_from_dir%\gdal\plugins-external\*.* %_to_dir%gdal\plugins-external\
+
+REM Copy gdal plugins-external
+xcopy /v /c /r /y %_from_dir%\gdal\plugins-optional\*.* %_to_dir%gdal\plugins-optional\
 
 REM Copy needed Tamas binaries:
-FOR %%G IN (cfitsio.dll freexl.dll geos.dll geos_c.dll hdf.dll hdf5.dll hdf5_hl.dll hdf5_cpp.dll hdf5_hl_cpp.dll libcrypto-1_1.dll libcrypto-1_1-x64.dll 
-			libcurl.dll libexpat.dll tiff.dll tiffxx.dll ogdi.dll mfhdf.dll  
-            iconv-2.dll libmysql.dll libpng16.dll libpq.dll libssl-1_1.dll libssl-1_1-x64.dll libxml2.dll lti_lidar_dsdk_1.1.dll netcdf.dll   
-            openjp2.dll spatialite.dll sqlite3.dll szip.dll tbb.dll xdr.dll zlib.dll  
+FOR %%G IN (cfitsio.dll freexl.dll geos.dll geos_c.dll hdf.dll hdf5.dll hdf5_hl.dll hdf5_cpp.dll hdf5_hl_cpp.dll libcrypto-1_1.dll libcrypto-1_1-x64.dll libcrypto-3-x64.dll
+			libcurl.dll libexpat.dll tiff.dll tiffxx.dll ogdi.dll mfhdf.dll pcre.dll
+            iconv-2.dll libmysql.dll libpng16.dll libpq.dll libssl-1_1.dll libssl-1_1-x64.dll libssl-3-x64.dll libxml2.dll lti_lidar_dsdk_1.1.dll netcdf.dll   
+            openjp2.dll proj_9.dll spatialite.dll sqlite3.dll szip.dll tbb.dll xdr.dll zlib.dll zstd.dll
             NCSEcw.dll) DO (
 	IF EXIST %_from_dir%\%%G (
     xcopy /v /c /r /y %_from_dir%\%%G  %_to_dir%
@@ -51,8 +64,8 @@ FOR %%G IN (cfitsio.dll freexl.dll geos.dll geos_c.dll hdf.dll hdf5.dll hdf5_hl.
 )
 
 REM gdal contains the version number, so use a wildcard:
-del /f /q %_to_dir%\gdal*.dll
-xcopy /v /c /r /y %_from_dir%\gdal3*.dll  %_to_dir%
+rem del /f /q %_to_dir%\gdal*.dll
+rem xcopy /v /c /r /y %_from_dir%\gdal3*.dll  %_to_dir%
 
 REM xerces contains a version number, so use a wildcard:
 del /f /q %_to_dir%\xerces-c*.dll
@@ -63,10 +76,18 @@ del /f /q %_to_dir%\lti_dsdk*.dll
 xcopy /v /c /r /y %_from_dir%\lti_dsdk*.dll  %_to_dir%
 
 REM Updates for GDAL v3:
-del /f /q %_to_dir%\gdal3*.dll
-xcopy /v /c /r /y %_from_dir%\gdal3*.dll  %_to_dir%
-del /f /q %_to_dir%\proj_7*.dll
-xcopy /v /c /r /y %_from_dir%\proj_7*.dll  %_to_dir%
+rem del /f /q %_to_dir%\gdal3*.dll
+rem xcopy /v /c /r /y %_from_dir%\gdal3*.dll  %_to_dir%
+rem del /f /q %_to_dir%\proj_7*.dll
+rem xcopy /v /c /r /y %_from_dir%\proj_7*.dll  %_to_dir%
+
+REM gdal witout the version number:
+del /f /q %_to_dir%\gdal.dll
+xcopy /v /c /r /y %_from_dir%\gdal.dll  %_to_dir%
+
+REM msvcp140*
+rem xcopy /v /c /r /y %_from_dir%\msvcp140.dll  %_to_dir%
+xcopy /v /c /r /y %_from_dir%\msvcp140*.dll  %_to_dir%
 
 REM Copy licenses:
 xcopy /v /c /r /y %_from_dir%\..\..\..\licenses\*.rtf %_to_dir%..\Licenses\
