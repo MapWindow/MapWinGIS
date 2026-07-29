cd350a737c94b404460684f7b81774c8de68151c
Daniel Hedén <daniel.heden@sweco.se>
2026-07-29 18:09:54 +0200

    Added mesasge pump helper to run AxMap tests with.

--- MapWinGisTests-net/MapWinGisTests/AxMapTests.cs
+++ MapWinGisTests-net/MapWinGisTests/AxMapTests.cs
@@ -14,112 +14,123 @@ public class AxMapTests
 		    StringComparison.OrdinalIgnoreCase);
 
 	public AxMapTests(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
-    
-    [StaFact]
-    public void VersionTest()
-    {
-        using var form = new WinFormsApp1.Form1();
-        form.ShouldNotBeNull();
-        form.EnsureMapControlCreated();
+	{
+		_testOutputHelper = testOutputHelper;
+	}
 
-        var version = form.GetMapWinGisVersion();
-        version.ShouldNotBeNull();
-        version.Major.ShouldBe(5);
-        version.Minor.ShouldBe(4);
-        version.Build.ShouldBeGreaterThanOrEqualTo(0);
-        _testOutputHelper.WriteLine("Version: {0}", version);
-    }
+	[StaFact]
+	public void VersionTest()
+	{
+		WinFormsTestRunner.Run(() =>
+		{
+			using var form = new WinFormsApp1.Form1();
+			form.ShouldNotBeNull();
+			form.EnsureMapControlCreated();
+
+			var version = form.GetMapWinGisVersion();
+			version.ShouldNotBeNull();
+			version.Major.ShouldBe(5);
+			version.Minor.ShouldBe(4);
+			version.Build.ShouldBeGreaterThanOrEqualTo(0);
+			_testOutputHelper.WriteLine("Version: {0}", version);
+		});
+	}
 
 	[StaFact]
 	public void MapProjectionTest()
 	{
-		using var form = new WinFormsApp1.Form1();
-		if(Environment.Is64BitProcess)
-			form.Show(); // We need to show the form to have a valid map control (x64)
-		form.ShouldNotBeNull();
-		form.EnsureMapControlCreated();
+		WinFormsTestRunner.Run(() =>
+		{
+			using var form = new WinFormsApp1.Form1();
+			if(Environment.Is64BitProcess)
+				form.Show(); // We need to show the form to have a valid map control (x64)
+			form.ShouldNotBeNull();
+			form.EnsureMapControlCreated();
 
-		var visible = !IsRunningOnGitHubActions; // Don't render when running on GitHub Actions
-		var sfLocation = Helpers.GetTestFilePath("UnitedStates-3857.shp");
-		_testOutputHelper.WriteLine($"MapProjectionTest() call form.OpenFile() with visible: {visible}");
-		var layerHandle = form.OpenFile(sfLocation, visible);
-		layerHandle.ShouldNotBe(-1, "form.OpenFile failed");
+			var visible = !IsRunningOnGitHubActions; // Don't render when running on GitHub Actions
+			var sfLocation = Helpers.GetTestFilePath("UnitedStates-3857.shp");
+			_testOutputHelper.WriteLine($"MapProjectionTest() call form.OpenFile() with visible: {visible}");
+			var layerHandle = form.OpenFile(sfLocation, visible);
+			layerHandle.ShouldNotBe(-1, "form.OpenFile failed");
 
-		var epsgCode = form.GetMapProjectionAsEpsgCode();
-		epsgCode.ShouldBe(3857);
+			var epsgCode = form.GetMapProjectionAsEpsgCode();
+			epsgCode.ShouldBe(3857);
+		});
 	}
 
-    [StaFact]
-    public void OpenShapefileWithInvalidSpatialIndex()
-    {
-        using var form = new WinFormsApp1.Form1();
-        form.ShouldNotBeNull();
-        var visible = !IsRunningOnGitHubActions; // Don't render when running on GitHub Actions
-        form.EnsureMapControlCreated();
+	[StaFact]
+	public void OpenShapefileWithInvalidSpatialIndex()
+	{
+		WinFormsTestRunner.Run(() =>
+		{
+			using var form = new WinFormsApp1.Form1();
+			form.ShouldNotBeNull();
+			var visible = !IsRunningOnGitHubActions; // Don't render when running on GitHub Actions
+			form.EnsureMapControlCreated();
 
-		var sfLocation = Helpers.GetTestFilePath("Issue-216.shp");
-		_testOutputHelper.WriteLine($"OpenShapefileWithInvalidSpatialIndex() call form.OpenFile() with visible: {visible}");
-		var layerHandle = form.OpenFile(sfLocation, visible);
-        layerHandle.ShouldNotBe(-1, "form.OpenFile failed");
+			var sfLocation = Helpers.GetTestFilePath("Issue-216.shp");
+			_testOutputHelper.WriteLine($"OpenShapefileWithInvalidSpatialIndex() call form.OpenFile() with visible: {visible}");
+			var layerHandle = form.OpenFile(sfLocation, visible);
+			layerHandle.ShouldNotBe(-1, "form.OpenFile failed");
 
-        var sf = form.GetShapefileFromLayer(layerHandle);
-        sf.ShouldNotBeNull("Could not get shapefile from layer");
+			var sf = form.GetShapefileFromLayer(layerHandle);
+			sf.ShouldNotBeNull("Could not get shapefile from layer");
 
-        // Test
-        sf.HasSpatialIndex.ShouldBeTrue();
-        sf.UseSpatialIndex.ShouldBeTrue();
-        sf.IsSpatialIndexValid().ShouldBeTrue();
-        sf.HasInvalidShapes().ShouldBeFalse();
-        sf.NumShapes.ShouldBe(13424);
-        sf.Extents.xMin.ShouldBe(108.722071, 0.00001);
-        sf.Extents.yMin.ShouldBe(34.149021, 0.00001);
-        sf.Extents.xMax.ShouldBe(109.139842, 0.00001);
-        sf.Extents.yMax.ShouldBe(34.457816, 0.00001);
-    }
+			// Test
+			sf.HasSpatialIndex.ShouldBeTrue();
+			sf.UseSpatialIndex.ShouldBeTrue();
+			sf.IsSpatialIndexValid().ShouldBeTrue();
+			sf.HasInvalidShapes().ShouldBeFalse();
+			sf.NumShapes.ShouldBe(13424);
+			sf.Extents.xMin.ShouldBe(108.722071, 0.00001);
+			sf.Extents.yMin.ShouldBe(34.149021, 0.00001);
+			sf.Extents.xMax.ShouldBe(109.139842, 0.00001);
+			sf.Extents.yMax.ShouldBe(34.457816, 0.00001);
+		});
+	}
 
-    [StaFact]
-    public void ShapefileKeyTest()
-    {
-        // AS mentioned at https://mapwindow.discourse.group/t/key-property-of-shape-object-not-work/1250
+	[StaFact]
+	public void ShapefileKeyTest()
+	{
+		// AS mentioned at https://mapwindow.discourse.group/t/key-property-of-shape-object-not-work/1250
 
-        using var form = new WinFormsApp1.Form1();
-        if (Environment.Is64BitProcess)
-        {
-            form.Show(); // We need to show the form to have a valid map control (x64)
-		}
-        form.ShouldNotBeNull();
-        form.EnsureMapControlCreated();
+		WinFormsTestRunner.Run(() =>
+		{
+			using var form = new WinFormsApp1.Form1();
+			if (Environment.Is64BitProcess)
+				form.Show(); // We need to show the form to have a valid map control (x64)
+	
+			form.ShouldNotBeNull();
+			form.EnsureMapControlCreated();
 
-        // Create shapefile:
-        var sfPolygon = Helpers.CreateTestPolygonShapefile();
+			// Create shapefile:
+			var sfPolygon = Helpers.CreateTestPolygonShapefile();
 
-        // Set key for shapefile:
-        const string sfKeyValue = "This is my sf key";
-        sfPolygon.Key = sfKeyValue;
-        // Check:
-        sfPolygon.Key.ShouldBe(sfKeyValue);
+			// Set key for shapefile:
+			const string sfKeyValue = "This is my sf key";
+			sfPolygon.Key = sfKeyValue;
+			// Check:
+			sfPolygon.Key.ShouldBe(sfKeyValue);
 
-        // Set key for first shape:
-        const string shpKeyValue = "This is my shp key";
-        sfPolygon.Shape[0].Key = shpKeyValue;
-        // Check:
-        sfPolygon.Shape[0].Key.ShouldBe(shpKeyValue);
-        var shp = sfPolygon.Shape[0];
-        shp.Key.ShouldBe(shpKeyValue);
+			// Set key for first shape:
+			const string shpKeyValue = "This is my shp key";
+			sfPolygon.Shape[0].Key = shpKeyValue;
+			// Check:
+			sfPolygon.Shape[0].Key.ShouldBe(shpKeyValue);
+			var shp = sfPolygon.Shape[0];
+			shp.Key.ShouldBe(shpKeyValue);
 
-        // Re-check:
-        sfPolygon.Key.ShouldBe(sfKeyValue);
+			// Re-check:
+			sfPolygon.Key.ShouldBe(sfKeyValue);
 
-		// Add shapefile to map and test again:
-		var visible = !IsRunningOnGitHubActions; // Don't render when running on GitHub Actions
-		_testOutputHelper.WriteLine($"ShapefileKeyTest() call form.AddShapefileToMap() with visible: {visible}");
-		var layerHandle = form.AddShapefileToMap(sfPolygon, visible);
-        // Get sf back:
-        var sf = form.GetShapefileFromLayer(layerHandle);
-        sf.Key.ShouldBe(sfKeyValue);
-        sf.Shape[0].Key.ShouldBe(shpKeyValue);
-    }
+			// Add shapefile to map and test again:
+			var visible = !IsRunningOnGitHubActions; // Don't render when running on GitHub Actions
+			_testOutputHelper.WriteLine($"ShapefileKeyTest() call form.AddShapefileToMap() with visible: {visible}");
+			var layerHandle = form.AddShapefileToMap(sfPolygon, visible);
+			// Get sf back:
+			var sf = form.GetShapefileFromLayer(layerHandle);
+			sf.Key.ShouldBe(sfKeyValue);
+			sf.Shape[0].Key.ShouldBe(shpKeyValue);
+		});
+	}
 }
---
+++ MapWinGisTests-net/MapWinGisTests/WinFormsTestRunner.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+
+namespace MapWinGisTests
+{
+	internal static class WinFormsTestRunner
+	{
+		public static void Run(Action action)
+		{
+			Exception? exception = null;
+
+			var thread = new Thread(() => {
+				try {
+					Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);
+
+					using var context = new ApplicationContext();
+					action();
+				} catch(Exception ex)
+				{
+					exception = ex;
+				}
+			});
+
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+			thread.Join();
+
+			if(exception != null)
+				throw exception;
+		}
+	}
+}
