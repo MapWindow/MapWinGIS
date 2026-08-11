a7de9e05b6bfad290535c47839e08cc96fe49ae5
Daniel Hedén <daniel.heden@sweco.se>
2026-08-11 14:25:02 +0200

    Implemented error handling in GlobalClassFactory and related COM object creation methods.

--- src/Utilities/ComHelper.cpp
+++ src/Utilities/ComHelper.cpp
@@ -32,7 +32,7 @@
 // Sets new instance of COM object to the given pointer. Takes care to release the old reference
 bool ComHelper::SetRef(IDispatch* newVal, IDispatch** oldVal, bool allowNull)
 {
-	if (*oldVal == NULL)
+	if (*oldVal == nullptr)
 	{
 		if (newVal)
 		{
@@ -42,7 +42,7 @@ bool ComHelper::SetRef(IDispatch* newVal, IDispatch** oldVal, bool allowNull)
 	}
 	else if ((*oldVal) != newVal)
 	{
-		if (newVal == NULL && !allowNull)
+		if (newVal == nullptr && !allowNull)
 		{
 			// NULL reference isn't allowed
 			return false;
@@ -52,7 +52,7 @@ bool ComHelper::SetRef(IDispatch* newVal, IDispatch** oldVal, bool allowNull)
 			if (*oldVal)
 			{
 				(*oldVal)->Release();
-				(*oldVal) = NULL;
+				(*oldVal) = nullptr;
 			}
 			(*oldVal) = newVal;
 
@@ -75,175 +75,175 @@ bool ComHelper::SetRef(IDispatch* newVal, IDispatch** oldVal, bool allowNull)
 HRESULT ComHelper::CreateInstance(tkInterface interfaceId, IDispatch** retVal)
 {
 	HRESULT result = S_FALSE;
-	void* val = NULL;
+	void* val = nullptr;
 	switch (interfaceId)
 	{
 		case tkInterface::idPoint:
-			ComHelper::CreatePoint((IPoint**)&val);
+			ComHelper::CreatePoint(reinterpret_cast<IPoint**>(&val));
 			break;
 		case tkInterface::idShape:
-			ComHelper::CreateShape((IShape**)&val);
+			ComHelper::CreateShape(reinterpret_cast<IShape**>(&val));
 			break;
 		case tkInterface::idExtents:
-			ComHelper::CreateExtents((IExtents**)&val);
+			ComHelper::CreateExtents(reinterpret_cast<IExtents**>(&val));
 			break;
 		case tkInterface::idField:
-			result = CoCreateInstance(CLSID_Field, NULL, CLSCTX_INPROC_SERVER, IID_IField, (void**)&val);
+			result = CoCreateInstance(CLSID_Field, nullptr, CLSCTX_INPROC_SERVER, IID_IField, &val);
 			break;
 		case tkInterface::idLabel:
-			result = CoCreateInstance(CLSID_Label, NULL, CLSCTX_INPROC_SERVER, IID_ILabel, (void**)&val);
+			result = CoCreateInstance(CLSID_Label, nullptr, CLSCTX_INPROC_SERVER, IID_ILabel, &val);
 			break;
 		case tkInterface::idChart:
-			result = CoCreateInstance(CLSID_Chart, NULL, CLSCTX_INPROC_SERVER, IID_IChart, (void**)&val);
+			result = CoCreateInstance(CLSID_Chart, nullptr, CLSCTX_INPROC_SERVER, IID_IChart, &val);
 			break;
 		case tkInterface::idChartField:
-			result = CoCreateInstance(CLSID_ChartField, NULL, CLSCTX_INPROC_SERVER, IID_IChartField, (void**)&val);
+			result = CoCreateInstance(CLSID_ChartField, nullptr, CLSCTX_INPROC_SERVER, IID_IChartField, &val);
 			break;
 		case tkInterface::idCharts:
-			result = CoCreateInstance(CLSID_Charts, NULL, CLSCTX_INPROC_SERVER, IID_ICharts, (void**)&val);
+			result = CoCreateInstance(CLSID_Charts, nullptr, CLSCTX_INPROC_SERVER, IID_ICharts, &val);
 			break;
 		case tkInterface::idColorScheme:
-			result = CoCreateInstance(CLSID_ColorScheme, NULL, CLSCTX_INPROC_SERVER, IID_IColorScheme, (void**)&val);
+			result = CoCreateInstance(CLSID_ColorScheme, nullptr, CLSCTX_INPROC_SERVER, IID_IColorScheme, &val);
 			break;
 		case tkInterface::idDrawingRectangle:
-			result = CoCreateInstance(CLSID_DrawingRectangle, NULL, CLSCTX_INPROC_SERVER, IID_IDrawingRectangle, (void**)&val);
+			result = CoCreateInstance(CLSID_DrawingRectangle, nullptr, CLSCTX_INPROC_SERVER, IID_IDrawingRectangle, &val);
 			break;
 		case tkInterface::idESRIGridManager:
-			result = CoCreateInstance(CLSID_ESRIGridManager, NULL, CLSCTX_INPROC_SERVER, IID_IESRIGridManager, (void**)&val);
+			result = CoCreateInstance(CLSID_ESRIGridManager, nullptr, CLSCTX_INPROC_SERVER, IID_IESRIGridManager, &val);
 			break;
 		case tkInterface::idExpression:
-			result = CoCreateInstance(CLSID_Expression, NULL, CLSCTX_INPROC_SERVER, IID_IExpression, (void**)&val);
+			result = CoCreateInstance(CLSID_Expression, nullptr, CLSCTX_INPROC_SERVER, IID_IExpression, &val);
 			break;
 		case tkInterface::idFieldStatOperations:
-			result = CoCreateInstance(CLSID_FieldStatOperations, NULL, CLSCTX_INPROC_SERVER, IID_IFieldStatOperations, (void**)&val);
+			result = CoCreateInstance(CLSID_FieldStatOperations, nullptr, CLSCTX_INPROC_SERVER, IID_IFieldStatOperations, &val);
 			break;
 		case tkInterface::idFileManager:
-			result = CoCreateInstance(CLSID_FileManager, NULL, CLSCTX_INPROC_SERVER, IID_IFileManager, (void**)&val);
+			result = CoCreateInstance(CLSID_FileManager, nullptr, CLSCTX_INPROC_SERVER, IID_IFileManager, &val);
 			break;
 		case tkInterface::idFunction:
-			result = CoCreateInstance(CLSID_Function, NULL, CLSCTX_INPROC_SERVER, IID_IFunction, (void**)&val);
+			result = CoCreateInstance(CLSID_Function, nullptr, CLSCTX_INPROC_SERVER, IID_IFunction, &val);
 			break;
 		case tkInterface::idGdalDataset:
-			result = CoCreateInstance(CLSID_GdalDataset, NULL, CLSCTX_INPROC_SERVER, IID_IGdalDataset, (void**)&val);
+			result = CoCreateInstance(CLSID_GdalDataset, nullptr, CLSCTX_INPROC_SERVER, IID_IGdalDataset, &val);
 			break;
 		case tkInterface::idGdalDriver:
-			result = CoCreateInstance(CLSID_GdalDriver, NULL, CLSCTX_INPROC_SERVER, IID_IGdalDriver, (void**)&val);
+			result = CoCreateInstance(CLSID_GdalDriver, nullptr, CLSCTX_INPROC_SERVER, IID_IGdalDriver, &val);
 			break;
 		case tkInterface::idGdalDriverManager:
-			result = CoCreateInstance(CLSID_GdalDriverManager, NULL, CLSCTX_INPROC_SERVER, IID_IGdalDriverManager, (void**)&val);
+			result = CoCreateInstance(CLSID_GdalDriverManager, nullptr, CLSCTX_INPROC_SERVER, IID_IGdalDriverManager, &val);
 			break;
 		case tkInterface::idGdalRasterBand:
-			result = CoCreateInstance(CLSID_GdalRasterBand, NULL, CLSCTX_INPROC_SERVER, IID_IGdalRasterBand, (void**)&val);
+			result = CoCreateInstance(CLSID_GdalRasterBand, nullptr, CLSCTX_INPROC_SERVER, IID_IGdalRasterBand, &val);
 			break;
 		case tkInterface::idGeoProjection:
-			result = CoCreateInstance(CLSID_GeoProjection, NULL, CLSCTX_INPROC_SERVER, IID_IGeoProjection, (void**)&val);
+			result = CoCreateInstance(CLSID_GeoProjection, nullptr, CLSCTX_INPROC_SERVER, IID_IGeoProjection, &val);
 			break;
 		case tkInterface::idGlobalSettings:
-			result = CoCreateInstance(CLSID_GlobalSettings, NULL, CLSCTX_INPROC_SERVER, IID_IGlobalSettings, (void**)&val);
+			result = CoCreateInstance(CLSID_GlobalSettings, nullptr, CLSCTX_INPROC_SERVER, IID_IGlobalSettings, &val);
 			break;
 		case tkInterface::idGrid:
-			result = CoCreateInstance(CLSID_Grid, NULL, CLSCTX_INPROC_SERVER, IID_IGrid, (void**)&val);
+			result = CoCreateInstance(CLSID_Grid, nullptr, CLSCTX_INPROC_SERVER, IID_IGrid, &val);
 			break;
 		case tkInterface::idGridColorBreak:
-			result = CoCreateInstance(CLSID_GridColorBreak, NULL, CLSCTX_INPROC_SERVER, IID_IGridColorBreak, (void**)&val);
+			result = CoCreateInstance(CLSID_GridColorBreak, nullptr, CLSCTX_INPROC_SERVER, IID_IGridColorBreak, &val);
 			break;
 		case tkInterface::idGridColorScheme:
-			result = CoCreateInstance(CLSID_GridColorScheme, NULL, CLSCTX_INPROC_SERVER, IID_IGridColorScheme, (void**)&val);
+			result = CoCreateInstance(CLSID_GridColorScheme, nullptr, CLSCTX_INPROC_SERVER, IID_IGridColorScheme, &val);
 			break;
 		case tkInterface::idGridHeader:
-			result = CoCreateInstance(CLSID_GridHeader, NULL, CLSCTX_INPROC_SERVER, IID_IGridHeader, (void**)&val);
+			result = CoCreateInstance(CLSID_GridHeader, nullptr, CLSCTX_INPROC_SERVER, IID_IGridHeader, &val);
 			break;
 		case tkInterface::idHistogram:
-			result = CoCreateInstance(CLSID_Histogram, NULL, CLSCTX_INPROC_SERVER, IID_IHistogram, (void**)&val);
+			result = CoCreateInstance(CLSID_Histogram, nullptr, CLSCTX_INPROC_SERVER, IID_IHistogram, &val);
 			break;
 		case tkInterface::idIdentifier:
-			result = CoCreateInstance(CLSID_Identifier, NULL, CLSCTX_INPROC_SERVER, IID_IIdentifier, (void**)&val);
+			result = CoCreateInstance(CLSID_Identifier, nullptr, CLSCTX_INPROC_SERVER, IID_IIdentifier, &val);
 			break;
 		case tkInterface::idImage:
-			result = CoCreateInstance(CLSID_Image, NULL, CLSCTX_INPROC_SERVER, IID_IImage, (void**)&val);
+			result = CoCreateInstance(CLSID_Image, nullptr, CLSCTX_INPROC_SERVER, IID_IImage, &val);
 			break;
 		case tkInterface::idLabelCategory:
-			result = CoCreateInstance(CLSID_LabelCategory, NULL, CLSCTX_INPROC_SERVER, IID_ILabelCategory, (void**)&val);
+			result = CoCreateInstance(CLSID_LabelCategory, nullptr, CLSCTX_INPROC_SERVER, IID_ILabelCategory, &val);
 			break;
 		case tkInterface::idLabels:
-			result = CoCreateInstance(CLSID_Labels, NULL, CLSCTX_INPROC_SERVER, IID_ILabels, (void**)&val);
+			result = CoCreateInstance(CLSID_Labels, nullptr, CLSCTX_INPROC_SERVER, IID_ILabels, &val);
 			break;
 		case tkInterface::idLinePattern:
-			result = CoCreateInstance(CLSID_LinePattern, NULL, CLSCTX_INPROC_SERVER, IID_ILinePattern, (void**)&val);
+			result = CoCreateInstance(CLSID_LinePattern, nullptr, CLSCTX_INPROC_SERVER, IID_ILinePattern, &val);
 			break;
 		case tkInterface::idLineSegment:
-			result = CoCreateInstance(CLSID_LineSegment, NULL, CLSCTX_INPROC_SERVER, IID_ILineSegment, (void**)&val);
+			result = CoCreateInstance(CLSID_LineSegment, nullptr, CLSCTX_INPROC_SERVER, IID_ILineSegment, &val);
 			break;
 		case tkInterface::idMeasuring:
-			result = CoCreateInstance(CLSID_Measuring, NULL, CLSCTX_INPROC_SERVER, IID_IMeasuring, (void**)&val);
+			result = CoCreateInstance(CLSID_Measuring, nullptr, CLSCTX_INPROC_SERVER, IID_IMeasuring, &val);
 			break;
 		case tkInterface::idOgrDatasource:
-			result = CoCreateInstance(CLSID_OgrDatasource, NULL, CLSCTX_INPROC_SERVER, IID_IOgrDatasource, (void**)&val);
+			result = CoCreateInstance(CLSID_OgrDatasource, nullptr, CLSCTX_INPROC_SERVER, IID_IOgrDatasource, &val);
 			break;
 		case tkInterface::idOgrLayer:
-			result = CoCreateInstance(CLSID_OgrLayer, NULL, CLSCTX_INPROC_SERVER, IID_IOgrLayer, (void**)&val);
+			result = CoCreateInstance(CLSID_OgrLayer, nullptr, CLSCTX_INPROC_SERVER, IID_IOgrLayer, &val);
 			break;	
 		case tkInterface::idSelectionList:
-			result = CoCreateInstance(CLSID_SelectionList, NULL, CLSCTX_INPROC_SERVER, IID_ISelectionList, (void**)&val);
+			result = CoCreateInstance(CLSID_SelectionList, nullptr, CLSCTX_INPROC_SERVER, IID_ISelectionList, &val);
 			break;
 		case tkInterface::idShapeDrawingOptions:
-			result = CoCreateInstance(CLSID_ShapeDrawingOptions, NULL, CLSCTX_INPROC_SERVER, IID_IShapeDrawingOptions, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapeDrawingOptions, nullptr, CLSCTX_INPROC_SERVER, IID_IShapeDrawingOptions, &val);
 			break;
 		case tkInterface::idShapeEditor:
-			result = CoCreateInstance(CLSID_ShapeEditor, NULL, CLSCTX_INPROC_SERVER, IID_IShapeEditor, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapeEditor, nullptr, CLSCTX_INPROC_SERVER, IID_IShapeEditor, &val);
 			break;
 		case tkInterface::idShapefile:
-			result = CoCreateInstance(CLSID_Shapefile, NULL, CLSCTX_INPROC_SERVER, IID_IShapefile, (void**)&val);
+			result = CoCreateInstance(CLSID_Shapefile, nullptr, CLSCTX_INPROC_SERVER, IID_IShapefile, &val);
 			break;
 		case tkInterface::idShapefileCategories:
-			result = CoCreateInstance(CLSID_ShapefileCategories, NULL, CLSCTX_INPROC_SERVER, IID_IShapefileCategories, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapefileCategories, nullptr, CLSCTX_INPROC_SERVER, IID_IShapefileCategories, &val);
 			break;
 		case tkInterface::idShapefileCategory:
-			result = CoCreateInstance(CLSID_ShapefileCategory, NULL, CLSCTX_INPROC_SERVER, IID_IShapefileCategory, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapefileCategory, nullptr, CLSCTX_INPROC_SERVER, IID_IShapefileCategory, &val);
 			break;
 #ifdef OLD_API
 		case tkInterface::idShapefileColorBreak:
-			result = CoCreateInstance(CLSID_ShapefileColorBreak, NULL, CLSCTX_INPROC_SERVER, IID_IShapefileColorBreak, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapefileColorBreak, nullptr, CLSCTX_INPROC_SERVER, IID_IShapefileColorBreak, &val);
 			break;
 		case tkInterface::idShapefileColorScheme:
-			result = CoCreateInstance(CLSID_ShapefileColorScheme, NULL, CLSCTX_INPROC_SERVER, IID_IShapefileColorScheme, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapefileColorScheme, nullptr, CLSCTX_INPROC_SERVER, IID_IShapefileColorScheme, &val);
 			break;
 #endif
 		case tkInterface::idShapeNetwork:
-			result = CoCreateInstance(CLSID_ShapeNetwork, NULL, CLSCTX_INPROC_SERVER, IID_IShapeNetwork, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapeNetwork, nullptr, CLSCTX_INPROC_SERVER, IID_IShapeNetwork, &val);
 			break;
 		case tkInterface::idShapeValidationInfo:
-			result = CoCreateInstance(CLSID_ShapeValidationInfo, NULL, CLSCTX_INPROC_SERVER, IID_IShapeValidationInfo, (void**)&val);
+			result = CoCreateInstance(CLSID_ShapeValidationInfo, nullptr, CLSCTX_INPROC_SERVER, IID_IShapeValidationInfo, &val);
 			break;
 		case tkInterface::idTable:
-			result = CoCreateInstance(CLSID_Table, NULL, CLSCTX_INPROC_SERVER, IID_ITable, (void**)&val);
+			result = CoCreateInstance(CLSID_Table, nullptr, CLSCTX_INPROC_SERVER, IID_ITable, &val);
 			break;
 		case tkInterface::idTileProviders:
-			result = CoCreateInstance(CLSID_TileProviders, NULL, CLSCTX_INPROC_SERVER, IID_ITileProviders, (void**)&val);
+			result = CoCreateInstance(CLSID_TileProviders, nullptr, CLSCTX_INPROC_SERVER, IID_ITileProviders, &val);
 			break;
 		case tkInterface::idTiles:
-			result = CoCreateInstance(CLSID_Tiles, NULL, CLSCTX_INPROC_SERVER, IID_ITiles, (void**)&val);
+			result = CoCreateInstance(CLSID_Tiles, nullptr, CLSCTX_INPROC_SERVER, IID_ITiles, &val);
 			break;
 		case tkInterface::idTin:
-			result = CoCreateInstance(CLSID_Tin, NULL, CLSCTX_INPROC_SERVER, IID_ITin, (void**)&val);
+			result = CoCreateInstance(CLSID_Tin, nullptr, CLSCTX_INPROC_SERVER, IID_ITin, &val);
 			break;
 		case tkInterface::idUndoList:
-			result = CoCreateInstance(CLSID_UndoList, NULL, CLSCTX_INPROC_SERVER, IID_IUndoList, (void**)&val);
+			result = CoCreateInstance(CLSID_UndoList, nullptr, CLSCTX_INPROC_SERVER, IID_IUndoList, &val);
 			break;
 		case tkInterface::idUtils:
-			result = CoCreateInstance(CLSID_Utils, NULL, CLSCTX_INPROC_SERVER, IID_IUtils, (void**)&val);
+			result = CoCreateInstance(CLSID_Utils, nullptr, CLSCTX_INPROC_SERVER, IID_IUtils, &val);
 			break;
 		case tkInterface::idVector:
-			result = CoCreateInstance(CLSID_Vector, NULL, CLSCTX_INPROC_SERVER, IID_IVector, (void**)&val);
+			result = CoCreateInstance(CLSID_Vector, nullptr, CLSCTX_INPROC_SERVER, IID_IVector, &val);
 			break;
 		case tkInterface::idWmsLayer:
-			result = CoCreateInstance(CLSID_WmsLayer, NULL, CLSCTX_INPROC_SERVER, IID_IWmsLayer, (void**)&val);
+			result = CoCreateInstance(CLSID_WmsLayer, nullptr, CLSCTX_INPROC_SERVER, IID_IWmsLayer, &val);
 			break;
 		case tkInterface::idGdalUtils:
-			result = CoCreateInstance(CLSID_GdalUtils, NULL, CLSCTX_INPROC_SERVER, IID_IGdalUtils, (void**)&val);
+			result = CoCreateInstance(CLSID_GdalUtils, nullptr, CLSCTX_INPROC_SERVER, IID_IGdalUtils, &val);
 			break;
 	}
-	*retVal = val ? (IDispatch*)val : NULL;
+	*retVal = val ? static_cast<IDispatch*>(val) : nullptr;
 	return result;
 }
 
@@ -308,7 +308,22 @@ CString ComHelper::GetInterfaceName(tkInterface id)
 // ********************************************************
 void ComHelper::CreatePoint(IPoint** point)
 {
-	m_factory.pointFactory->CreateInstance(NULL, IID_IPoint, (void**)point);
+	if (point == nullptr)
+	{
+		return;
+	}
+	
+	*point = nullptr;
+
+	if (m_factory.IsPointFactoryAvailable())
+	{
+		m_factory.pointFactory->CreateInstance(nullptr, IID_IPoint, reinterpret_cast<void**>(point));
+	}
+	else
+	{
+		// Fallback to CoCreateInstance if factory is not available
+		CoCreateInstance(CLSID_Point, nullptr, CLSCTX_INPROC_SERVER, IID_IPoint, reinterpret_cast<void**>(point));
+	}
 }
 
 // ********************************************************
@@ -316,7 +331,22 @@ void ComHelper::CreatePoint(IPoint** point)
 // ********************************************************
 void ComHelper::CreateShape(IShape** shp)
 {
-	m_factory.shapeFactory->CreateInstance(NULL, IID_IShape, (void**)shp);
+	if (shp == nullptr)
+	{
+		return;
+	}
+
+	*shp = nullptr;
+
+	if (m_factory.IsShapeFactoryAvailable())
+	{
+		m_factory.shapeFactory->CreateInstance(nullptr, IID_IShape, reinterpret_cast<void**>(shp));
+	}
+	else
+	{
+		// Fallback to CoCreateInstance if factory is not available
+		CoCreateInstance(CLSID_Shape, nullptr, CLSCTX_INPROC_SERVER, IID_IShape, reinterpret_cast<void**>(shp));
+	}
 }
 
 // ********************************************************
@@ -324,5 +354,20 @@ void ComHelper::CreateShape(IShape** shp)
 // ********************************************************
 void ComHelper::CreateExtents(IExtents** box)
 {
-	m_factory.extentsFactory->CreateInstance(NULL, IID_IExtents, (void**)box);
+	if (box == nullptr)
+	{
+		return;
+	}
+
+	*box = nullptr;
+
+	if (m_factory.IsExtentsFactoryAvailable())
+	{
+		m_factory.extentsFactory->CreateInstance(nullptr, IID_IExtents, reinterpret_cast<void**>(box));
+	}
+	else
+	{
+		// Fallback to CoCreateInstance if factory is not available
+		CoCreateInstance(CLSID_Extents, nullptr, CLSCTX_INPROC_SERVER, IID_IExtents, reinterpret_cast<void**>(box));
+	}
 }
--- src/Utilities/GlobalClassFactory.h
+++ src/Utilities/GlobalClassFactory.h
@@ -2,26 +2,99 @@
 
 class GlobalClassFactory
 {
-public:	
+public:
 	IClassFactory* pointFactory;
 	IClassFactory* shapeFactory;
 	IClassFactory* extentsFactory;
 
+private:
+	HRESULT m_pointFactoryStatus;
+	HRESULT m_shapeFactoryStatus;
+	HRESULT m_extentsFactoryStatus;
+
+public:
 	GlobalClassFactory::GlobalClassFactory()
+		: pointFactory(nullptr)
+		, shapeFactory(nullptr)
+		, extentsFactory(nullptr)
+		, m_pointFactoryStatus(S_FALSE)
+		, m_shapeFactoryStatus(S_FALSE)
+		, m_extentsFactoryStatus(S_FALSE)
 	{
-		pointFactory = shapeFactory = extentsFactory =  NULL;
-		CoGetClassObject(CLSID_Point, CLSCTX_INPROC_SERVER, NULL, IID_IClassFactory, (void**)&pointFactory); 
-		CoGetClassObject(CLSID_Shape, CLSCTX_INPROC_SERVER, NULL, IID_IClassFactory, (void**)&shapeFactory);
-		CoGetClassObject(CLSID_Extents, CLSCTX_INPROC_SERVER, NULL, IID_IClassFactory, (void**)&extentsFactory);
+		// Try to get class factories, but don't crash if they fail
+		m_pointFactoryStatus = CoGetClassObject(CLSID_Point, CLSCTX_INPROC_SERVER, nullptr, IID_IClassFactory, reinterpret_cast<void**>(&pointFactory));
+		if (FAILED(m_pointFactoryStatus))
+		{
+			pointFactory = nullptr;
+		}
+
+		m_shapeFactoryStatus = CoGetClassObject(CLSID_Shape, CLSCTX_INPROC_SERVER, nullptr, IID_IClassFactory, reinterpret_cast<void**>(&shapeFactory));
+		if (FAILED(m_shapeFactoryStatus))
+		{
+			shapeFactory = nullptr;
+		}
+
+		m_extentsFactoryStatus = CoGetClassObject(CLSID_Extents, CLSCTX_INPROC_SERVER, nullptr, IID_IClassFactory, reinterpret_cast<void**>(&extentsFactory));
+		if (FAILED(m_extentsFactoryStatus))
+		{
+			extentsFactory = nullptr;
+		}
 	}
 
 	~GlobalClassFactory()
 	{
 		if (pointFactory)
-			pointFactory->Release(); 
+		{
+			pointFactory->Release();
+			pointFactory = nullptr;
+		}
 		if (shapeFactory)
+		{
 			shapeFactory->Release();
+			shapeFactory = nullptr;
+		}
 		if (extentsFactory)
+		{
 			extentsFactory->Release();
+			extentsFactory = nullptr;
+		}
+	}
+
+	// Check if all factories are available
+	BOOL IsInitialized() const
+	{
+		return pointFactory != nullptr && shapeFactory != nullptr && extentsFactory != nullptr;
+	}
+
+	// Check if a specific factory is available
+	BOOL IsPointFactoryAvailable() const
+	{
+		return pointFactory != nullptr;
+	}
+
+	BOOL IsShapeFactoryAvailable() const
+	{
+		return shapeFactory != nullptr;
+	}
+
+	BOOL IsExtentsFactoryAvailable() const
+	{
+		return extentsFactory != nullptr;
+	}
+
+	// Get error status for debugging
+	HRESULT GetPointFactoryStatus() const
+	{
+		return m_pointFactoryStatus;
+	}
+
+	HRESULT GetShapeFactoryStatus() const
+	{
+		return m_shapeFactoryStatus;
+	}
+
+	HRESULT GetExtentsFactoryStatus() const
+	{
+		return m_extentsFactoryStatus;
 	}
 };
